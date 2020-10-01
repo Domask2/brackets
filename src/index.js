66bbedfe9e0baf914a232c62e836b643d53fee34
Domask <domask2@mail.ru>
2020-10-01 14:41:36 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  let result = [];
+  
+ for(let i=0;i<str.length; i++) {
+    
+   for(let j=0;j<bracketsConfig.length; j++) {
+    
+     if(str[i] === bracketsConfig[j][0]) {
+        result.push(str[i]);
+     } else if (str[i] === bracketsConfig[j][1]) {
+       if(result[result.length-1] === bracketsConfig[j][0])
+       result.pop();
+     }
+     
+     if(bracketsConfig[j][0] === bracketsConfig[j][1] && str[i] === bracketsConfig[j][0]) {
+       if(result[result.length-2] === str[i] && result[result.length-2]!=undefined){
+         console.log(result)
+          result.pop();    
+          result.pop();
+       }
+     }
+   }
+ }
+  return !result.length
 }
